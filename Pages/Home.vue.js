13efe13236635af30c48a0b096f2778ff48a0ea4
Jonah <47265356+swiftsoul1@users.noreply.github.com>
2020-05-20 21:06:30 -0600

    Deleted classes

--- Pages/Home.vue.js
+++ Pages/Home.vue.js
@@ -8,13 +8,13 @@ var Home = {
 	},
 	template:  `<div class="container-fluid p-0 m-0">
                     <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 bg-tertiary align-self-top m-0 p-5">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4 bg-tertiary m-0 p-5">
                             <p class="font-bold" v-html="page.title">
                             </p>
                         <div v-html="page.body"></div>
                     
                         </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 m-0 p-0 bg-black align-self-center img-container">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 m-0 p-0 bg-black img-container">
                             <img :src="page.featured.source" class="img-fluid  p-0 pl-5 m-0 w-100 fixed-height-2"/>
                         <div class="img-offset" >
                             <img :src="page.featured.source2" class="img-logo"/>
