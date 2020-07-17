0ed3e1e1a9e5c05271129294c9085006492019ec
Jonah <47265356+swiftsoul1@users.noreply.github.com>
2020-07-16 22:01:23 -0600

    font awesome icon

--- site.js
+++ site.js
@@ -6,7 +6,7 @@ var prevScrollpos = window.pageYOffset;
 document.getElementById("fixed-button").style.visibility = "hidden";
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset
-    if (($(document).height()*.85) > currentScrollPos) {
+    if (($(document).height()*.75) > currentScrollPos) {
         document.getElementById("fixed-button").style.visibility = "hidden";
     } 
     else {
