c7c8500604537073a00c304c4d53ad86c5cdbab0
Rianne <9632612+littlepandabear@users.noreply.github.com>
2020-07-23 20:48:11 -0600

    images

--- Components/Credits.vue.js
+++ Components/Credits.vue.js
@@ -6,11 +6,11 @@ var Credits = {
                 <div class="col p-0 mb-3" v-for="credit,i in credits">
                      <div class="text-left">
                         <span v-if='credit.title' class='font-weight-bolder color-white'>{{credit.title}}<br></span>
-                        <span class="color-white h6" v-if="credit.artist">{{credit.artist}}<br></span>
-                        <span class="color-white h6" v-if="credit.date">{{credit.date}}<br></span>
-                        <span class="color-white h6" v-if="credit.location">{{credit.location}}<br></span>
-                        <span class="color-white h6" v-if="credit.medium">{{credit.medium}}<br></span>
-                        <span class="color-white h6" v-if="credit.collection">{{credit.collection}}<br></span>
+                        <span class="color-white font-weight-light" v-if="credit.artist">{{credit.artist}}<br></span>
+                        <span class="color-white font-weight-light" v-if="credit.date">{{credit.date}}<br></span>
+                        <span class="color-white font-weight-light" v-if="credit.location">{{credit.location}}<br></span>
+                        <span class="color-white font-weight-light" v-if="credit.medium">{{credit.medium}}<br></span>
+                        <span class="color-white font-weight-light" v-if="credit.collection">{{credit.collection}}<br></span>
                         <span><a v-if='credit.link' class='color-dark-tan font-weight-bolder' target="_blank" :href='credit.link'>Learn More</a></span>
                     </div>
                 </div>
